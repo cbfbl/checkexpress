7f4b36a6d19ce8e948ffdb1ea2cad7508bbb4be7
cbfbl <cbfbl651@gmail.com>
2018-09-02 14:36:55 +0300

    added genre update post and get
    genre form did not change

--- genreController.js
+++ genreController.js
@@ -149,10 +149,42 @@ exports.genre_delete_post = function(req, res) {
 
 // Display Genre update form on GET.
 exports.genre_update_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre update GET');
+    res.render('genre_form', { title: 'Update Genre' });
 };
 
 // Handle Genre update on POST.
 exports.genre_update_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre update POST');
+    // Validate that the name field is not empty.
+    body('name', 'Genre name required').isLength({ min: 1 }).trim(),
+    
+    // Sanitize (trim and escape) the name field.
+    sanitizeBody('name').trim().escape(),
+
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        // Create a genre object with escaped and trimmed data.
+        var genre = new Genre(
+            { 
+                name: req.body.name,
+                _id:req_params.id
+            }
+        );
+
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render the form again with sanitized values/error messages.
+            res.render('genre_form', { title: 'Create Genre', genre: genre, errors: errors.array()});
+        return;
+        }
+        else {
+            Genre.findByIdAndUpdate(req.params.id,genre,{},function(err,thegenre){
+                if(err){return next(err);}
+                res.redirect(thegenre.url);
+            });
+        }
+    }
 };
